04782d603ac2890a94137b496d8c7d82b4358c50
Ace <omarace95@gmail.com>
2024-03-04 14:03:16 -0800

    added visitor list

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -11,6 +11,7 @@ use std::io::stdin;
 
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
+
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
@@ -20,7 +21,18 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
+    let visitor_list = ["bert", "steve", "fred"];
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {}", name);
+    let mut allow_them_in = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+}
+        }
+    if allow_them_in {
+        println!("Welcome to the Treehouse, {}", name);
+    } else {
+        println!("Sorry, you aren't on the list.");
+    }
 }
