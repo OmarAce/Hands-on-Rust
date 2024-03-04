52220225e6276e06cc7a406e6aa84425efcc6c9b
Ace <omarace95@gmail.com>
2024-03-04 14:20:48 -0800

    structs

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,13 +1,22 @@
 use std::io::stdin;
 
-// fn main() {
-//     println!("Hello, what's your name?");
-//     let mut your_name = String::new();
-//     stdin()
-//         .read_line(&mut your_name)
-//         .expect("Failed to read line");
-//     println!("Hello, {}", your_name);
-//     }
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
 
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
@@ -21,18 +30,21 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let visitor_list = ["bert", "steve", "fred"];
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve","Hi Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
     println!("Hello, what's your name?");
     let name = what_is_your_name();
     let mut allow_them_in = false;
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_them_in = true;
-}
-        }
-    if allow_them_in {
-        println!("Welcome to the Treehouse, {}", name);
-    } else {
-        println!("Sorry, you aren't on the list.");
+
+    let known_visitor = visitor_list
+        .iter()
+        .find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You are not on the visitor list. Please leave.")
     }
 }
