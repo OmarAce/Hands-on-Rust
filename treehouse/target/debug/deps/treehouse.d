f6d510db2a34315d7b53ba9e21f6383f89d3ac7c
Ace <omarace95@gmail.com>
2024-03-04 13:43:19 -0800

    treehouse example

---
+++ treehouse/target/debug/deps/treehouse.d
@@ -0,0 +1,5 @@
+C:\Users\aweso\Desktop\Repositories\Hands-on-Rust\treehouse\target\debug\deps\treehouse.exe: src\main.rs
+
+C:\Users\aweso\Desktop\Repositories\Hands-on-Rust\treehouse\target\debug\deps\treehouse.d: src\main.rs
+
+src\main.rs:
---
+++ treehouse/target/debug/treehouse.d
@@ -0,0 +1,1 @@
+C:\Users\aweso\Desktop\Repositories\Hands-on-Rust\treehouse\target\debug\treehouse.exe: C:\Users\aweso\Desktop\Repositories\Hands-on-Rust\treehouse\src\main.rs
