11519f568fe54ebd58bdbb7559316e6d27047644
Ace <omarace95@gmail.com>
2024-03-03 16:40:58 -0800

    Hello World Test

---
+++ HelloWorld/testrust/target/debug/deps/testrust.d
@@ -0,0 +1,5 @@
+C:\Users\aweso\Desktop\Repositories\Hands-on-Rust\HelloWorld\testrust\target\debug\deps\testrust.exe: src\main.rs
+
+C:\Users\aweso\Desktop\Repositories\Hands-on-Rust\HelloWorld\testrust\target\debug\deps\testrust.d: src\main.rs
+
+src\main.rs:
---
+++ HelloWorld/testrust/target/debug/testrust.d
@@ -0,0 +1,1 @@
+C:\Users\aweso\Desktop\Repositories\Hands-on-Rust\HelloWorld\testrust\target\debug\testrust.exe: C:\Users\aweso\Desktop\Repositories\Hands-on-Rust\HelloWorld\testrust\src\main.rs
