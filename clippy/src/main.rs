16350f4e99c70c13f43f84071cfa53a1bd1d6bce
Ace <omarace95@gmail.com>
2024-03-03 16:53:44 -0800

    iterating without range

--- clippy/src/main.rs
+++ clippy/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let MYLIST = [ "One", "Two", "Three" ];
-    for i in 0..3 {
-        println!("{}", MYLIST[i]);
+    let my_list = [ "One", "Two", "Three" ];
+    for num in &my_list {
+        println!("{}", num);
     }
 }
